38142088f5f7e178efca200c95508b1d05164eb9
Jens_Nilsen <jensnilsen89@gmail.com>
2020-03-04 12:40:43 +0100

    mongoose added

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
-//
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/mendlybackend"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
@@ -15,7 +20,7 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello worlden')
 })
 
 // Start the server
